72324ab8a40a27b3e9371665ccc48e957dc8ab8d
Josh Slebodnik <slebodnik88@gmail.com>
2019-01-29 20:18:11 -0600

    Added Home Event List
    I added a tableview in a UIViewController and set all the delegates. I populated the events into an array using Alamofire and EVReflection.

--- Event/HomeViewController.swift
+++ Event/HomeViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 import Alamofire
+import SDWebImage
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
     
     var eventArray: [Event] = []
 
@@ -28,6 +31,7 @@ class HomeViewController: UIViewController {
             if events != nil {
                 self.eventArray = events!
                 print(self.eventArray)
+                self.tableView.reloadData()
             } else {
                 print("Event Nil")
             }
@@ -47,18 +51,63 @@ class HomeViewController: UIViewController {
 //        }
 //    }
     
-    func loadEventList(completion: @escaping ([Event]?) -> Void){
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventArray.count
+    }
     
-            Alamofire.request(EventRouter.events).responseString { response in
-                guard response.result.isSuccess,
-                    let value = response.result.value else {
-                        print("Error while getting events: \(String(describing: response.result.error))")
-                        completion(nil)
-                        return
-                }
-                completion([Event](json: value))
-            }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        let event = eventArray[indexPath.row]
+        
+        //Sets Event Image on Cell
+        if let eventImageView = cell?.contentView.viewWithTag(1) as? UIImageView {
+            eventImageView.sd_setImage(with: URL(string: event.image_url) )
         }
+        
+        //Event Title
+        if let titleLabel = cell?.contentView.viewWithTag(2) as? UILabel {
+            titleLabel.text = event.title
+        }
+        
+        //Date
+        if let dateLabel = cell?.contentView.viewWithTag(3) as? UILabel {
+            
+            let dateFormatterGet = DateFormatter()
+            dateFormatterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ssz"
+            let dateFormatterPrint = DateFormatter()
+            dateFormatterPrint.dateFormat = "MM/dd/yyyy ha"
+            let dateFormatterPrintHA = DateFormatter()
+            dateFormatterPrintHA.dateFormat = "ha"
+            
+            //Get Start Date
+            let startDate = dateFormatterGet.date(from: event.start_date_time)
+            let startDateString = dateFormatterPrint.string(from: startDate!)
+            
+            //Get End Date
+            let endDate = dateFormatterGet.date(from: event.end_date_time)
+            let endDateString = dateFormatterPrintHA.string(from: endDate!)
+            
+            dateLabel.text = startDateString + " - " + endDateString
+            
+        }
+        
+        return cell!
+    }
+    
+    func loadEventList(completion: @escaping ([Event]?) -> Void){
+        
+        Alamofire.request(EventRouter.events).responseString { response in
+            guard response.result.isSuccess,
+                let value = response.result.value else {
+                    print("Error while getting events: \(String(describing: response.result.error))")
+                    completion(nil)
+                    return
+            }
+            completion([Event](json: value))
+        }
+    }
 
 
 }
